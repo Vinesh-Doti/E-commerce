6fbea081b88bfb603373d086ba16f79663687e7d
Vinesh-Doti <165304202+Vinesh-Doti@users.noreply.github.com>
2025-08-30 16:21:06 +0530

    Update ProductDisplay.jsx

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,38 +1,34 @@
-import React, { useContext } from 'react'
-import './ProductDisplay.css'
-import star_icon from '../Assets/star_icon.png'
+import React, { useContext } from 'react';
+import './ProductDisplay.css';
+import star_icon from '../Assets/star_icon.png';
 import star_dull_icon from "../Assets/star_dull_icon.png";
 import { ShopContext } from '../../Context/ShopContext';
 
-const BACKEND_URL = 'https://e-commerce-backend-plor.onrender.com';
-
 const ProductDisplay = (props) => {
-    const {product} = props;
-    const {addToCart} = useContext(ShopContext);
-
-    const productImage = `${BACKEND_URL}/images/${product.image}`;
+    const { product } = props;
+    const { addToCart } = useContext(ShopContext);
 
     return (
         <div className='productdisplay'>
             <div className="productdisplay-left">
                 <div className="productdisplay-img-list">
-                    <img src={productImage} alt="" />
-                    <img src={productImage} alt="" />
-                    <img src={productImage} alt="" />
-                    <img src={productImage} alt="" />
+                    <img src={product.image} alt={product.name} />
+                    <img src={product.image} alt={product.name} />
+                    <img src={product.image} alt={product.name} />
+                    <img src={product.image} alt={product.name} />
                 </div>
                 <div className="productdisplay-img">
-                    <img className='productdisplay-main-img' src={productImage} alt="" />
+                    <img className='productdisplay-main-img' src={product.image} alt={product.name} />
                 </div>
             </div>
             <div className="productdisplay-right">
                 <h1>{product.name}</h1>
                 <div className="productdisplay-right-star">
-                    <img src={star_icon} alt="" />
-                    <img src={star_icon} alt="" />
-                    <img src={star_icon} alt="" />
-                    <img src={star_icon} alt="" />
-                    <img src={star_dull_icon} alt="" />
+                    <img src={star_icon} alt="star" />
+                    <img src={star_icon} alt="star" />
+                    <img src={star_icon} alt="star" />
+                    <img src={star_icon} alt="star" />
+                    <img src={star_dull_icon} alt="star" />
                     <p>(122)</p>
                 </div>
                 <div className="productdisplay-right-prices">
@@ -56,12 +52,12 @@ const ProductDisplay = (props) => {
                         <div>XXL</div>
                     </div>
                 </div>
-                <button onClick={() => { addToCart(product.id) }}>ADD TO CART</button>
+                <button onClick={() => addToCart(product.id)}>ADD TO CART</button>
                 <p className='productdisplay-right-category'><span>Category:</span> Women, T-Shirt, Crop Top</p>
                 <p className='productdisplay-right-category'><span>Tags:</span> Modern, Latest</p>
             </div>
         </div>
-    )
+    );
 }
 
 export default ProductDisplay;
