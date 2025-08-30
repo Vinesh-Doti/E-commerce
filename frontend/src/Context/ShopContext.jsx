dda53020f767ebe2d668c03b9ee3e80b6a54d8c3
Vinesh-Doti <165304202+Vinesh-Doti@users.noreply.github.com>
2025-08-30 17:52:26 +0530

    Update ShopContext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { getImageUrl } from '../../../utils/getImageUrl'; // ✅ Import utility
+import { getImageUrl } from '../utils/getImageUrl';
+
 
 // Create context
 export const ShopContext = createContext(null);
