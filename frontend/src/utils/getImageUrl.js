5a1e6759ac86cf710e073ae5be8fcf78314d98cb
Vinesh-Doti <165304202+Vinesh-Doti@users.noreply.github.com>
2025-08-30 18:05:18 +0530

    Update getImageUrl.js

--- frontend/src/utils/getImageUrl.js
+++ frontend/src/utils/getImageUrl.js
@@ -1,9 +1,6 @@
-// frontend/src/utils/getImageUrl.js
-
-const BACKEND_URL = 'https://e-commerce-backend-plor.onrender.com';
-
 export const getImageUrl = (imagePath) => {
-  // If imagePath already starts with http(s), return as-is
+  const BACKEND_URL = 'https://e-commerce-backend-plor.onrender.com';
   if (!imagePath) return '';
   return imagePath.startsWith('http') ? imagePath : `${BACKEND_URL}/images/${imagePath}`;
 };
+
